c8e291e4a7b9a0d4760022935c84fe80fbc752e6
Zeynep Küden <78428090+Zeynep9966@users.noreply.github.com>
2021-05-01 05:02:30 +0300

    Initial commit

---
+++ Folium.py
@@ -0,0 +1,15 @@
+import folium
+import pandas as pd
+import xlrd
+
+
+map = folium.Map(location=[41,29],tiles="CartoDB positron")
+
+veri = pd.read_excel("tr-cities.xlsx")
+
+
+ilceler = list(veri["City"])
+enlemler = list(veri["Enlem"])
+boylamlar = list(veri["Boylam"])
+
+print(enlemler)
